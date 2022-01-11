8a3f6b82d3ec949e18f4defbba126519455ca586
Yevhen <92868360+ekodenko@users.noreply.github.com>
2022-01-11 23:21:32 +0200

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Admin on 11.01.2022.
+//  Copyright © 2022 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
